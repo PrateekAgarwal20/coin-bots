5092f3c69d0a09a9ac61444e8fc1e4b3a4a29461
Prateek-Agarwal-20 <prateeka@sas.upenn.edu>
2017-07-12 19:20:16 -0700

    initial webpack configurations

---
+++ app/app.js
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+import App from './components/App';
+
+ReactDOM.render(<App/>,
+   document.getElementById('root'));
---
+++ app/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+      <div>Your App Here</div>
+    )
+  }
+}
+
+export default App;
